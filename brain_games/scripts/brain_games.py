8bde1050a6c485d1c4da3819efbb2925b562e043
ShMariya <shidrikova.ma@gmail.com>
2022-11-15 20:29:37 +0500

    linter correction

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def greet():
     print("Welcome to the Brain Games!")
-    
+
+
 def main():
     greet()
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-    
-
-
-# from brain_games.cli import welcome_user
-
-# def main():
-#     print("Welcome to the Brain Games!")
-    
-# def main():
-#     print("Welcome to the Brain Games!")
-
-# if __name__ == '__main__':
-#     main()
-    
-# welcome_user()
