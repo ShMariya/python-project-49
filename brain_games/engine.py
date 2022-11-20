d68740f029e64191e7c44ec76c89fb7d68be2250
ShMariya <shidrikova.ma@gmail.com>
2022-11-20 16:51:53 +0500

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,34 +7,11 @@ def welcome_user():
     return name
 
 
-# task = 'What is the result of the expression?'
-# question = '5 + 6'
-# correct_answer = str(11)
-
-
 def greet(name):
     print(f'Hello, {name}!')
     return name
 
 
-# greet(welcome_user())
-# print(greet(welcome_user()))
-
-
-
-# def generate_question():#generate of a question to the user
-#     number1 = randint(1, 50)
-#     number2 = randint(1, 50)
-#     operators = ['+', '-', '*']
-#     operator = random.choice(operators)
-#     question = f'{number1} {operator} {number2}'
-#     if operator == '+':
-#         correct_answer = str(number1 + number2)
-#     elif operator == '-':
-#         correct_answer = str(number1 - number2)
-#     elif operator == '*':
-#         correct_answer = str(number1 * number2)
-#     return question, correct_answer
 def generate_question():
     pass
 
@@ -55,5 +32,3 @@ Let's try again, {name}!")
             i += 1
         if i == 3:
             print(f'Congratulations, {name}!')
-    
-# rounds(greet(welcome_user()), task, generate_question())
