a6f088cbba633bf84cc088c5681065dbaae86889
ShMariya <shidrikova.ma@gmail.com>
2022-11-21 20:02:17 +0500

    import modules brain_games.games.calc_game and brain_games.games.even_game to engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,23 +1,27 @@
 import prompt
 import random
 from random import randint
+import brain_games.games.calc_game
+import brain_games.games.even_game
 
-def welcome_user():
+
+def welcome_user(): #узнаем имя игрока
     name = prompt.string('May I have your name? ')
     return name
 
 
-def greet(name):
+def greet(name): #приветствие игрока
     print(f'Hello, {name}!')
     return name
 
 
-def generate_question():
-    pass
-
-def rounds(name, task, question_correct_answer):
+def rounds(name, task, question_correct_answer, game):
     print(task)
     for i in range(3):
+        if game == 'calc':
+            question_correct_answer = brain_games.games.calc_game.generate_question_calc()
+        elif game == 'even':
+            question_correct_answer = brain_games.games.even_game.generate_question_even()
         question, correct_answer = question_correct_answer
         print(question)
         answer = input()
@@ -32,3 +36,17 @@ Let's try again, {name}!")
             i += 1
         if i == 3:
             print(f'Congratulations, {name}!')
+
+
+def calc():
+    
+    task = 'What is the result of the expression?'
+    # question, correct_answer = generate_question()
+    rounds(greet(welcome_user()), task, brain_games.games.calc_game.generate_question_calc(), 'calc')
+    
+
+def is_even():
+    
+    task = 'Answer "yes" if the number is even, otherwise answer "no".'
+    # question, correct_answer = generate_question()
+    rounds(greet(welcome_user()), task, brain_games.games.even_game.generate_question_even(), 'even')
