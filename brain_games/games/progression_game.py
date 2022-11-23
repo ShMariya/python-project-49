c41d8b34136058d369b7737b31a70c318c31af0d
ShMariya <shidrikova.ma@gmail.com>
2022-11-23 17:48:10 +0500

    add brain_games/games/progression_game.pybrain_games/scripts/brain_progression.py

---
+++ brain_games/games/progression_game.py
@@ -0,0 +1,21 @@
+
+import random
+from random import randint
+
+task = 'What number is missing in the progression?'
+game = 'progression?'
+
+def generate_question_progress():#generate of a question to the user
+    first_member = randint(1, 5)
+    step = randint(1, 5)
+    progression = [first_member]
+    for i in range(10):
+        progression.append(first_member + step*(i + 1))
+        i += 1
+    correct_answer = random.choice(progression)
+    progression[progression.index(correct_answer)] = '..'
+    question = ''
+    for i in progression:
+        question += str(i) + ' '
+    return question.strip(), str(correct_answer)
+
