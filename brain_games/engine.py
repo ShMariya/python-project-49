6921d52012dfaf1485932ae770ff75e5f1401539
ShMariya <shidrikova.ma@gmail.com>
2022-11-26 13:12:26 +0500

    update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,26 +1,19 @@
 import prompt
 
 
-def greet(game):
-    print('Welcome to the Brain Games!\nMay I have your name?', end=' ')
-    name = input()
+def start_game(game):
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.task)
-    return name
-
-
-def start_game(game):
-    name = greet(game)
     for i in range(3):
         question, correct_answer = game.generate_question()
         print(question)
         answer = prompt.string('Your answer: ')
         if answer != correct_answer:
-            print(f"'{answer}' is wrong answer ;(. \
-Correct answer was '{correct_answer}'. \
-Let's try again, {name}!")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '\
+{correct_answer}'. Let's try again, {name}!")
             break
-
         else:
             print('Correct!')
             i += 1
