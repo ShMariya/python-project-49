cccdc5390c34b1f9da5de58ceeec0718cdeff8fb
ShMariya <shidrikova.ma@gmail.com>
2022-11-28 21:05:45 +0500

    update engine.py  - fix start_game

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,15 +5,14 @@ def start_game(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game.task)
+    print(game.TASK)
     for i in range(3):
-        question, correct_answer = game.generate_question()
+        question, correct_answer = game.get_question_correct_answer()
         print(question)
         answer = prompt.string('Your answer: ')
         if answer != correct_answer:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '\
+            return print(f"'{answer}' is wrong answer ;(. Correct answer was '\
 {correct_answer}'. Let's try again, {name}!")
-            break
         else:
             print('Correct!')
             i += 1
