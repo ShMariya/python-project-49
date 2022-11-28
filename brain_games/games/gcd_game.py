cefdb5764d026df858d7ad875f2b2a33e652f917
ShMariya <shidrikova.ma@gmail.com>
2022-11-28 21:13:16 +0500

    update brain_games/games/gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -9,5 +9,5 @@ def get_question_correct_answer():  # generate of a question to the user
     random_number1 = randint(1, 50)
     random_number2 = randint(1, 50)
     question_gcd = f'Question: {random_number1} {random_number2}'
-    correct_answer_gcd = str(math.gcd(random_number1, random_number1))
+    correct_answer_gcd = str(math.gcd(random_number1, random_number2))
     return question_gcd, correct_answer_gcd
